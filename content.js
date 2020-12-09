2b742d38a1f8f5d7fc3a200b2563b9328dc8158c
dvoroniuc <denisvoroniuc@gmail.com>
2020-12-09 13:21:33 +0200

    connected ui with api

--- content.js
+++ content.js
@@ -1,6 +1,29 @@
+async function getData(id) {
+  const response = await fetch(`http://localhost:8081/api/user/${id}`, {
+    method: 'GET',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+  });
+  return await response.json();
+}
+
 const newMessage = (message, sender, sendResponse) => {
   console.log(sender);
   console.log(sendResponse);
+
+  getData(message.id)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   const inputs = document.querySelectorAll(`input`);
   const textAreas = document.querySelectorAll(`textarea`);
 
--- popup.js
+++ popup.js
@@ -1,14 +1,57 @@
+async function postData(data = {}) {
+  const response = await fetch('http://localhost:8081/api/user', {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+  return await response.json();
+}
+
 document.addEventListener(
   'DOMContentLoaded',
   () => {
     const getDataItem = () => document.querySelector('#item').value;
 
     const newMessage = () => {
-      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, getDataItem());
+      postData({
+        email: 'email',
+        password: 'password',
+        personalData: data,
+      }).then((data) => {
+        chrome.tabs.query(
+          { currentWindow: true, active: true },
+          function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, data);
+          }
+        );
       });
     };
-    document.querySelector('button').addEventListener('click', newMessage);
+
+    const data = [];
+    const addData = () => {
+      if (getDataItem() !== '') {
+        data.push(getDataItem());
+        document.querySelector('#error').style.display = 'none';
+        document.querySelector('#item').value = '';
+
+        const fields = document.querySelector('#fields');
+        const field = data.map((v) => {
+          return `${v} <br/>`;
+        });
+        fields.innerHTML = field;
+      } else {
+        document.querySelector('#error').style.display = 'block';
+      }
+    };
+    document.querySelector('#add').addEventListener('click', addData);
+    document.querySelector('#send').addEventListener('click', newMessage);
   },
   false
 );
